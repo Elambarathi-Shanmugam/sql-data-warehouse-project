449dc467905a0ce1f0ba6fa0916604c816bc4ae5
Elambarathi-Shanmugam <selambarathi@gmail.com>
2026-03-04 19:30:18 +0530

    Create quality_checks_silver.sql

---
+++ tests/quality_checks_silver.sql
@@ -0,0 +1,108 @@
+*/
+  ======================================================================
+  Quality Checks
+  ======================================================================
+  Script Purpose:
+       This script helps to check the data qualities by checking
+              -Null or duplicates primary keys.
+              -Unwanted spaces in string fields.
+              -Data standardization and consistency
+              -Invalid data ranges and orders
+              -Data consistency between related fields.
+
+  Usage Notes:
+      -Run these checks after data loading silver layer.
+      -Investigate and resolve any discrepancies found during the checks.
+  =====================================================================
+  /*
+
+
+==============================================
+-- Check Duplicates and Nulls in Primary Key
+==============================================
+SELECT 
+   prd_id,
+   COUNT(*)
+   FROM silver.crm_prd_info
+   GROUP BY prd_id
+   HAVING COUNT(*) > 1 OR prd_id IS NULL
+  
+=====================================
+-- Check and remove Unwanted Spaces
+=====================================
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+  
+==================================
+-- Check Nulls and -ve Numbers
+==================================  
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 or  prd_cost IS NULL
+  
+=======================================
+--Data Standardization and Consistency
+=======================================  
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+  
+================================
+--Check For Invalid Date Orders
+================================  
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+  
+===============================
+-- For Silver_TABLE-3
+--Check Invalid Date Orders
+===============================
+  
+SELECT sls_ship_dt 
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt <= 0 OR LEN(sls_ship_dt) != 8
+OR sls_ship_dt < 19000101
+OR sls_ship_dt > 20500101
+  
+=======================
+--Check Invalid Date
+=======================  
+SELECT *
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+  
+===============================================
+-- Sales = Quantity * Price
+-- Values must not be 0 , Null or Negative
+===============================================  
+SELECT DISTINCT
+   sls_sales,
+   sls_quantity,
+   sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price OR
+sls_sales <= 0 OR sls_quantity <=0 OR sls_price <= 0
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+ORDER BY sls_sales, sls_quantity, sls_price
+
+SELECT DISTINCT
+     sls_sales,
+     sls_quantity,
+     sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price OR
+sls_sales <= 0 OR sls_quantity <=0 OR sls_price <= 0
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+ORDER BY sls_sales, sls_quantity, sls_price
+  
+========================================
+-- DATA Check for silver.erp_cust_az12
+========================================  
+SELECT DISTINCT
+gen
+FROM silver.erp_cust_az12
+WHERE bdate > GETDATE() AND
+cid like 'NAS%'
+SELECT * FROM silver.erp_cust_az12
+FROM silver.crm_sales_details
